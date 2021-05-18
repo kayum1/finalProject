4e87526e2db5d6a53489745c477ef1266c21668f
Kayum Amzad <kayum2@gmail.com>
2021-05-18 15:39:47 -0400

    Files added

--- src/const.js
+++ src/const.js
@@ -1,2 +1,2 @@
-export const URL = 'http://www.mapquestapi.com/directions/v2/route';
+export const URL = 'https://www.mapquestapi.com/directions/v2/route';
 export const API_KEY = 'h6rCO41hU3HtEXUQIr8509hv2zA9x3um';
